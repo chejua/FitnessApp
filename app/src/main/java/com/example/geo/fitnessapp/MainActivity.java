0fd9041240a9fd827201d048070c466d5b71b86d
Geo <ggaleano2014@fau.edu>
2016-06-07 18:15:44 -0400

    Change the color of the logo

--- app/src/main/java/com/example/geo/fitnessapp/MainActivity.java
+++ app/src/main/java/com/example/geo/fitnessapp/MainActivity.java
@@ -16,75 +16,37 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.menu);
-//
-//        ImageView imageBart = (ImageView) findViewById(R.id.imageView);
-//        RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.logoViewScreen);
-//        //imageBart.setTranslationX(-1000f);
-//
-//        //imageBart.animate().translationXBy(1000f).setDuration(5000);
-//        //imageBart.animate().scaleX(0.5f).scaleY(0.5f).setDuration(8000);
-//        //imageBart.animate().rotationX(180f).setDuration(8000);
-//        relativeLayout.animate().alpha(0.0f).setStartDelay(2000).setDuration(3000);
-//
-//        final GridLayout gridLayout = (GridLayout)findViewById(R.id.gridLayout);
-//
-//        CountDownTimer timer = new CountDownTimer(5000, 1000){
-//
-//            @Override
-//            public void onTick(long millisUntilFinished) {
-//
-//            }
-//
-//            @Override
-//            public void onFinish() {
-//
-//                gridLayout.setVisibility(View.VISIBLE);
-//
-//
-//            }
-//        }.start();
-//
-//
-//
-//
+        setContentView(R.layout.activity_main);
+
+        ImageView logo = (ImageView) findViewById(R.id.imageView);
+        RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.logoViewScreen);
+        //imageBart.setTranslationX(-1000f);
+        //imageBart.animate().translationXBy(1000f).setDuration(5000);
+        //imageBart.animate().scaleX(0.5f).scaleY(0.5f).setDuration(8000);
+        //imageBart.animate().rotationX(180f).setDuration(8000);
+        relativeLayout.animate().alpha(0.0f).setStartDelay(2000).setDuration(3000);
+
+        final GridLayout gridLayout = (GridLayout)findViewById(R.id.gridLayout);
+
+        CountDownTimer timer = new CountDownTimer(5000, 1000){
+
+            @Override
+            public void onTick(long millisUntilFinished) {
+
+            }
+
+            @Override
+            public void onFinish() {
+
+                gridLayout.setVisibility(View.VISIBLE);
+
+
+            }
+        }.start();
+
+
+
+
   }
 }
 
-
-//<GridLayout
-//android:layout_width="match_parent"
-//        android:layout_height="match_parent"
-//        android:layout_alignParentTop="true"
-//        android:id="@+id/gridLayout"
-//        android:layout_alignParentLeft="true"
-//        android:layout_alignParentStart="true">
-//
-//
-//<Button
-//android:layout_width="wrap_content"
-//        android:layout_height="wrap_content"
-//        android:text="Programs"
-//        android:textSize="20sp"
-//        android:id="@+id/button0"
-//        android:layout_row="0"
-//        android:layout_column="0"
-//        android:layout_gravity="fill"
-//        android:layout_rowWeight="1"
-//        android:layout_columnWeight="1"
-//        android:background="#d97703" />
-//
-//<Button
-//android:layout_width="wrap_content"
-//        android:layout_height="wrap_content"
-//        android:text="Nutrition Programs"
-//        android:textSize="20sp"
-//        android:id="@+id/button2"
-//        android:layout_row="1"
-//        android:layout_column="0"
-//        android:layout_gravity="fill"
-//        android:layout_rowWeight="1"
-//        android:layout_columnWeight="1"
-//        android:background="#d97703" />
-//
-//</GridLayout>
